15adf189f2e162a5e72942ac2d4dc33290d36be1
nitron99 <nitin99bop2cod2f@gmail.com>
2021-07-15 12:20:25 +0530

    final

--- src/components/homepage/homepage.js
+++ src/components/homepage/homepage.js
@@ -1,5 +1,10 @@
 import React ,{ Component }  from "react";
 import './homepage.css';
+import './projects.css';
+
+import {ReactComponent as Worldmap} from './worldmap.svg';
+import {ReactComponent as Arpanpic} from './arpan_pic.svg';
+import {ReactComponent as Sendbtn} from './send_white_48dp.svg';
 
 import Navbar from '../navbar/navbar';
 import Footer from '../footer/footer';
@@ -12,36 +17,102 @@ class homepage extends Component {
                 <div className="card">
                     
                     <Navbar />
-
                     <h1 className="background-Hello">HELLO</h1>
-                    <div className="content">
-                        I'm <em>Arpan Das</em>, a
-                        <p className="main_title">FULL-STACK<br/><em>WEB DEVELOPER</em></p>
-                        I am a passionate person looking for opportunities to grow my knowlegde and <br/>expriences related to my field.
+                    <div className="frontdetail">
+                        <Arpanpic className="arpan-pic"/>
+                        
+                        <div className="content">
+                            I'm <em>Arpan Das</em>, a
+                            <p className="main_title">FULL-STACK<br/><em>WEB DEVELOPER</em></p>
+                            I like to do things in the field of Application Development, but I also want to go into other fields like Investment Banking. My aim is to do something <em>different</em> in my life.
 
-                        <div className="contacts">
-                            <div className="email">
-                                Email<br/><em>dasarpan471@gmail.com</em>
-                            </div>
+                            <div className="contacts">
+                                <div className="email">
+                                    Email<br/><em>dasarpan471@gmail.com</em>
+                                </div>
 
-                            <div className="mobile">
-                                Phone<br/><em><a href="tel:+91 7668798433" id="mobiles">+91 7668798433</a></em>
+                                <div className="mobile">
+                                    Phone<br/><em><a href="tel:+91 7668798433" id="mobiles">+91 7668798433</a></em>
+                                </div>
+
                             </div>
 
                         </div>
-
+                        
                     </div>
 
-                    <h1 className="project-title">PROJECTS</h1>
+                    <div className="project-box">
+                        <h1 className="project-title">PROJECTS</h1>
+                        <div className="project1">
+                                
+                            <div className="project1-content">
+                            <div className="project1-no">01/05</div>
+                            <div className="project1-title">Tesla clone</div>
+                                    
+                                   This project is developed by using <em>styled components</em>.
 
 
 
 
+                            </div>
+                            <div className="project1img">
+                                    
 
 
-                    <h1 className="contactme-title">CONTACT ME</h1>
+
+                            </div>
 
 
+
+
+                        </div>
+
+
+                        <div className="project2">
+                                 
+                            <div className="project2-content">
+                                <div className="project2-no">02/05</div>
+                                <div className="project2-title">Chat bot</div>
+                                        
+                                This is project developed by using <em>nodejs</em> and <em>socket.io</em>.
+
+
+
+
+                                </div>
+                                <div className="project2img">
+                                        
+
+
+
+                                </div>
+          
+                        </div>
+
+                    </div>
+                    
+                    <div className="contactme-box">
+                        <h1 className="contactme-title">CONTACT ME</h1>
+                        <div className="contactme-content">
+                            <div className="contactme-text">
+                                Let's get in touch <em>Today.</em>
+
+                                <input className="input" type="text" placeholder="Email...">
+                                    
+                                </input>
+
+                                <Sendbtn className="send_btn" />
+
+
+                            </div>
+                            <div className="worldmap">
+                                <Worldmap />
+
+
+
+                            </div>
+                        </div>
+                    </div>
                     <Footer />
 
                 </div>
